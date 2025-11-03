8b35c7a0b5ebe4bdfd26f8a1bdb2b5a0a6f6df35
MuhammadArman2007 <mdarmansiddiqui9011@gmail.com>
2025-11-04 04:30:45 +0530

    first commit

--- src/Divide.java
+++ src/Divide.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Divide {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Welcome to our Divide Calculator");
+        System.out.println("Enter your first number:");
+        int a=sc.nextInt();
+        System.out.println("Enter you second number:");
+        int b=sc.nextInt();
+        System.out.println("The Division of two number is: " +(a/b));
+    }
 }
